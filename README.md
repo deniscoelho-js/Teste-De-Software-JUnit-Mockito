c9b5b58d6d85dc230d115f6ab27abfe33f3a98fc
Denis Coelho <deniscoelho.dev@gmail.com>
2022-06-30 21:40:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Testes em Java | JUnit | Mockito | TDD
+
+## Projeto
+
+Criação de baterias de testes unitários em Java. Aplicação de TDD, código limpo, percentual de aceitação e cobertura de código.
+
+## Metodologia
+
+- [x] TDD
+- [x] Builders de objetos
+- [x] Código limpo
+- [x] Métodos de testes isolados
+- [x] Métricas de qualidade
+
+## Tecnologias
+
+Este projeto foi desenvolvido utilizando as seguintes tecnologias:
+
+- [x] Java
+- [x] JUnit
+- [x] Mockito
+
+## Instalação e uso
+
+Clone o repositório
+
+```bash
+git clone https://github.com/deniscoelho-js/Teste-De-Software-JUnit-Mockito.git
+
+```
---
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -0,0 +1,45 @@
+package br.ce.wcaquino.servicos;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		return locacao;
+	}
+
+	public static void main(String[] args) {
+//		cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("usuario 1");
+		Filme filme = new Filme("filme 1", 2, 5.5);
+		
+//		ação
+		Locacao locacao =  service.alugarFilme(usuario, filme);
+		
+//		verificacao
+		System.out.println(locacao.getValor());
+		System.out.println(locacao.getDataLocacao());
+		System.out.println(locacao.getDataRetorno());
+	}
+}
